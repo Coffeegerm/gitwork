de0ec8aff2e601aee15bd32768592d86400a0228
David <dyarzebinski97@gmail.com>
2020-09-06 17:06:26 -0400

    styling

--- src/components/Jobs.js
+++ src/components/Jobs.js
@@ -3,6 +3,7 @@ import { useJobs } from "../hooks/useJobs";
 import { paginationContext } from "../context/paginationContext";
 import { jobSearchContext } from "../context/jobSearchContext";
 import { Link } from "react-router-dom";
+import { Pagination } from "../components/Pagination";
 
 export const Jobs = (props) => {
   const { pageNumber } = useContext(paginationContext);
@@ -26,42 +27,46 @@ export const Jobs = (props) => {
     <div>
       {error && <h1>Error getting jobs. {error.message}</h1>}
       {isLoading && <h4>Loading</h4>}
-      {data &&
-        data.map((job, i) => (
-          <div
-            className="max-w-lg mx-auto flex p-6 bg-white rounded-lg shadow-x mb-2 mt-2"
-            key={i}
-          >
-            <div className="flex-shrink-0">
-              <img
-                className="h-12 w-12"
-                src={job.company_logo}
-                alt="Company Logo"
-              />
-            </div>
-            <div className="ml-6 pt-1">
-              <p className="text-base text-gray-600 leading-normal">
-                {job.company}
-              </p>
-              <Link to={`/details/${job.id}`}>
-                <h4 className="text-xl text-gray-900 leading-tight">
-                  {job.title}
-                </h4>
-              </Link>
+      {data && (
+        <div>
+          {data.map((job, i) => (
+            <div
+              className="mx-auto items-stretch flex p-6 bg-white rounded-lg shadow-x mb-2 mt-2"
+              key={i}
+            >
+              <div className="flex-shrink-0">
+                <img
+                  className="h-12 w-12"
+                  src={job.company_logo}
+                  alt="Company Logo"
+                />
+              </div>
+              <div className="ml-6 pt-1">
+                <p className="text-base text-gray-600 leading-normal">
+                  {job.company}
+                </p>
+                <Link to={`/details/${job.id}`}>
+                  <h4 className="text-xl text-gray-900 leading-tight">
+                    {job.title}
+                  </h4>
+                </Link>
 
-              <div className="flex flex-row justify-between">
-                <div>
-                  {job.type === "Full Time" ? "Full time" : "Part time"}
-                </div>
+                <div className="flex flex-row justify-between">
+                  <div>
+                    {job.type === "Full Time" ? "Full time" : "Part time"}
+                  </div>
 
-                <div className="flex flex-row">
-                  <div>{job.location}</div>
-                  <div>{getNumberOfDaysSincePost(job.created_at)}</div>
+                  <div className="flex flex-row">
+                    <div>{job.location}</div>
+                    <div>{getNumberOfDaysSincePost(job.created_at)}</div>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        ))}
+          ))}
+          <Pagination />
+        </div>
+      )}
     </div>
   );
 };
--- src/pages/landing.js
+++ src/pages/landing.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { Sidebar, Jobs, Page } from "../components";
-import { Pagination } from "../components/Pagination";
 
 export const Landing = () => {
   const [onlyFulltime, setOnlyFulltime] = useState(false);
@@ -18,14 +17,15 @@ export const Landing = () => {
           </button>
         </div>
       </div>
-      <div className="grid grid-cols-2 lg:grid-cols-1">
-        <Sidebar
-          onlyFulltime={onlyFulltime}
-          setOnlyFulltime={setOnlyFulltime}
-        />
-        <div>
+      <div className="flex flex-row sm:flex-col lg:flex-row">
+        <div className="flex-1">
+          <Sidebar
+            onlyFulltime={onlyFulltime}
+            setOnlyFulltime={setOnlyFulltime}
+          />
+        </div>
+        <div style={{ flexBasis: "66%" }}>
           <Jobs onlyFulltime={onlyFulltime} />
-          <Pagination />
         </div>
       </div>
     </Page>
